b604090dc6beb8e5a5330de3aab36162612fc0b4
Luludad33 <597961231@qq.com>
2026-08-08 18:54:19 +0800

    fix: move UNUserNotificationCenterDelegate to NSObject subclass

--- 20-20-20护眼/App.swift
+++ 20-20-20护眼/App.swift
@@ -2,19 +2,13 @@ import SwiftUI
 import UserNotifications
 
 @main
-struct EyeCareApp: App, UNUserNotificationCenterDelegate {
+struct EyeCareApp: App {
     @StateObject private var timerManager = TimerManager()
     @Environment(\.scenePhase) private var scenePhase
+    private let notificationDelegate = NotificationDelegate()
 
     init() {
-        UNUserNotificationCenter.current().delegate = self
-    }
-
-    // 前台也展示通知横幅并播放自定义声音
-    func userNotificationCenter(_ center: UNUserNotificationCenter,
-                                willPresent notification: UNNotification,
-                                withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-        completionHandler([.banner, .sound, .list])
+        UNUserNotificationCenter.current().delegate = notificationDelegate
     }
 
     var body: some Scene {
@@ -25,5 +19,15 @@ struct EyeCareApp: App, UNUserNotificationCenterDelegate {
                     timerManager.handleScenePhaseChange(to: newPhase)
                 }
         }
+    }
+}
+
+// UNUserNotificationCenterDelegate 是类协议，必须由 NSObject 子类实现
+final class NotificationDelegate: NSObject, UNUserNotificationCenterDelegate {
+    // 前台也展示通知横幅并播放自定义声音
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                willPresent notification: UNNotification,
+                                withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.banner, .sound, .list])
     }
 }
